9bb5ebb53f747d668b6e103358ddc58d218657ac
Michal Dziubich <michal.dziubich@gmail.com>
2020-09-26 10:17:08 +0200

    dodanie cyklu zycia componentu do app.components

--- adv/src/app/app.component.ts
+++ adv/src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'adv';
+export class AppComponent
+  implements
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
+  title = 'SDA-angular';
+  ngOnChanges(e) {
+    console.log('ngOnChanges', e);
+  }
+  ngOnInit() {
+    console.log('ngOnInit');
+  }
+  ngDoCheck() {
+    console.log('ngDoCheck');
+  }
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit');
+  }
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked');
+  }
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+  }
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked');
+  }
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
+  }
 }
